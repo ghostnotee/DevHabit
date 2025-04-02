6090ed2f6972cf11162cb9d10007a6a0a03752a1
ghostNoté <selcukbilgen@hotmail.com>
2025-04-03 00:58:18 +0300

    Adding the Users Resource Completed. Add User entity and enhance Habits and Tags APIs. Introduce User DbSet in ApplicationDbContext, update HabitsController to support HATEOAS links based on Accept header, and refactor TagsController to include link generation for tags.

--- DevHabit.Api/Controllers/TagsController.cs
+++ DevHabit.Api/Controllers/TagsController.cs
@@ -1,49 +1,108 @@
+﻿using System.Net.Mime;
 using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Common;
 using DevHabit.Api.DTOs.Tags;
 using DevHabit.Api.Entities;
+using DevHabit.Api.Services;
 using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Api.Controllers;
 
 [ApiController]
 [Route("tags")]
-public class TagsController(ApplicationDbContext dbContext) : ControllerBase
+[Produces(
+    MediaTypeNames.Application.Json,
+    CustomMediaTypeNames.Application.JsonV1,
+    CustomMediaTypeNames.Application.HateoasJson,
+    CustomMediaTypeNames.Application.HateoasJsonV1)]
+public sealed class TagsController(ApplicationDbContext dbContext, LinkService linkService) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<TagsCollectionDto>> GetTags()
+    public async Task<ActionResult<TagsCollectionDto>> GetTags([FromHeader] AcceptHeaderDto acceptHeader)
     {
         List<TagDto> tags = await dbContext
             .Tags
             .Select(TagQueries.ProjectToDto())
             .ToListAsync();
-        var habitsCollectionDto = new TagsCollectionDto { Items = tags };
+
+        var habitsCollectionDto = new TagsCollectionDto
+        {
+            Items = tags
+        };
+
+        if (acceptHeader.IncludeLinks)
+        {
+            habitsCollectionDto.Links = CreateLinksForTags();
+        }
+
         return Ok(habitsCollectionDto);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<TagDto>> GetTag(string id)
+    public async Task<ActionResult<TagDto>> GetTag(string id, [FromHeader] AcceptHeaderDto acceptHeader)
     {
         TagDto? tag = await dbContext
             .Tags
             .Where(h => h.Id == id)
             .Select(TagQueries.ProjectToDto())
             .FirstOrDefaultAsync();
-        if (tag is null) return NotFound();
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
+        if (acceptHeader.IncludeLinks)
+        {
+            tag.Links = CreateLinksForTag(id);
+        }
+
         return Ok(tag);
     }
 
     [HttpPost]
-    public async Task<ActionResult<TagDto>> CreateTag(CreateTagDto createTagDto, IValidator<CreateTagDto> validator)
+    public async Task<ActionResult<TagDto>> CreateTag(
+        CreateTagDto createTagDto,
+        [FromHeader] AcceptHeaderDto acceptHeader,
+        IValidator<CreateTagDto> validator,
+        ProblemDetailsFactory problemDetailsFactory)
     {
-        await validator.ValidateAndThrowAsync(createTagDto);
+        ValidationResult validationResult = await validator.ValidateAsync(createTagDto);
+
+        if (!validationResult.IsValid)
+        {
+            ProblemDetails problem = problemDetailsFactory.CreateProblemDetails(
+                HttpContext,
+                StatusCodes.Status400BadRequest);
+            problem.Extensions.Add("errors", validationResult.ToDictionary());
+
+            return BadRequest(problem);
+        }
+
         Tag tag = createTagDto.ToEntity();
+
         if (await dbContext.Tags.AnyAsync(t => t.Name == tag.Name))
-            return Problem($"The tag '{tag.Name}' already exists", statusCode: StatusCodes.Status409Conflict);
+        {
+            return Problem(
+                detail: $"The tag '{tag.Name}' already exists",
+                statusCode: StatusCodes.Status409Conflict);
+        }
+
         dbContext.Tags.Add(tag);
+
         await dbContext.SaveChangesAsync();
+
         TagDto tagDto = tag.ToDto();
+
+        if (acceptHeader.IncludeLinks)
+        {
+            tagDto.Links = CreateLinksForTag(tag.Id);
+        }
+
         return CreatedAtAction(nameof(GetTag), new { id = tagDto.Id }, tagDto);
     }
 
@@ -51,9 +110,16 @@ public class TagsController(ApplicationDbContext dbContext) : ControllerBase
     public async Task<ActionResult> UpdateTag(string id, UpdateTagDto updateTagDto)
     {
         Tag? tag = await dbContext.Tags.FirstOrDefaultAsync(h => h.Id == id);
-        if (tag is null) return NotFound();
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
         tag.UpdateFromDto(updateTagDto);
+
         await dbContext.SaveChangesAsync();
+
         return NoContent();
     }
 
@@ -61,9 +127,39 @@ public class TagsController(ApplicationDbContext dbContext) : ControllerBase
     public async Task<ActionResult> DeleteTag(string id)
     {
         Tag? tag = await dbContext.Tags.FirstOrDefaultAsync(h => h.Id == id);
-        if (tag is null) return NotFound();
+
+        if (tag is null)
+        {
+            return NotFound();
+        }
+
         dbContext.Tags.Remove(tag);
+
         await dbContext.SaveChangesAsync();
+
         return NoContent();
+    }
+
+    private List<LinkDto> CreateLinksForTags()
+    {
+        List<LinkDto> links =
+        [
+            linkService.Create(nameof(GetTags), "self", HttpMethods.Get),
+            linkService.Create(nameof(CreateTag), "create", HttpMethods.Post)
+        ];
+
+        return links;
+    }
+
+    private List<LinkDto> CreateLinksForTag(string id)
+    {
+        List<LinkDto> links =
+        [
+            linkService.Create(nameof(GetTag), "self", HttpMethods.Get, new { id }),
+            linkService.Create(nameof(UpdateTag), "update", HttpMethods.Put, new { id }),
+            linkService.Create(nameof(DeleteTag), "delete", HttpMethods.Delete, new { id })
+        ];
+
+        return links;
     }
 }
