c06c6a8448b497078e9c8911a6918727a9ff4944
ghostNoté <selcukbilgen@hotmail.com>
2025-04-15 00:16:33 +0300

    Update CreateTagDtoValidator to enhance validation rules. Extend Description maximum length to 500 characters and enforce a maximum length for Name.

--- DevHabit.Api/DTOs/Tags/CreateTagDtoValidator.cs
+++ DevHabit.Api/DTOs/Tags/CreateTagDtoValidator.cs
@@ -6,7 +6,7 @@ public sealed class CreateTagDtoValidator : AbstractValidator<CreateTagDto>
 {
     public CreateTagDtoValidator()
     {
-        RuleFor(dto => dto.Name).NotEmpty().MinimumLength(3);
-        RuleFor(dto => dto.Description).MaximumLength(50);
+        RuleFor(x => x.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+        RuleFor(x => x.Description).MaximumLength(500);
     }
 }
